abb94187b5e3e564dd60e1d6b08eeba8350bf6a4
Luís Gustavo <luisgsilva270@gmail.com>
2022-07-22 00:15:51 +0000

    refactor and add class Person

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from pattern_matching import match_func
+from data_class import Person
 
 
 if __name__ == '__main__':
-    print("Hello World")
+    print(f"Hello {__file__} :0")
     match_func(1)
+    person = Person("Pedro", 22)
+    print(person.__str__())
