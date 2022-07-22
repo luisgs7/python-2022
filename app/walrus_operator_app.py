4470ba78ecbea9f3a2fde91f29e677f5b4c80b87
Luís Gustavo <luisgsilva270@gmail.com>
2022-07-22 18:37:43 +0000

    refactor messages

--- app/walrus_operator_app.py
+++ app/walrus_operator_app.py
@@ -3,8 +3,8 @@ from typing import List
 
 def walrus_operator(index: List[int]) -> None:
     if(a := len(index)) > 2:
-        print(f"Esta lista é maior que 2, ela tem:{a} números inteiros")
+        print(f"Esta lista contém:{a} números")
     elif(a := len(index)) < 2:
-        print(f"Esta lista é menor que 2, ela eh:{a} números inteiros")
+        print(f"Esta lista possui:{a} números")
     else:
-        print(f"Esta lista tem: {len(index)} números inteiros")
+        print(f"Esta lista tem: {len(index)} números")
