c94753d2782293bac259c357c3f7d7b6f43d050d
Luís Gustavo <luisgsilva270@gmail.com>
2022-07-22 01:24:11 +0000

    import module typing_app

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from pattern_matching_app import match_func
 from data_class_app import new_person
 from enum_app import hello_city
 from fstring_app import hi_fstring
+from typing_app import type_app
 
 
 if __name__ == '__main__':
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     new_person()
     hello_city()
     hi_fstring()
+    type_app([1, 2, 3], (4.1, 5.2, 6.3))
